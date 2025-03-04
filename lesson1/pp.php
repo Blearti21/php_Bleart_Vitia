2ed58c71994168d961b643829646d199b41de0dd
Blearti21 <bleartvitia91@gmail.com>
2025-03-04 20:47:42 +0100

    dAY1

---
+++ lesson1/pp.php
@@ -0,0 +1,24 @@
+<?php 
+   
+   $School='Digital School';
+   $number1='120';
+   $number2='50';
+
+   $perqindje1='10';
+   $perqindje2='3';
+
+
+
+   echo $number1+$number2;
+   echo "<br>";
+   echo $number1*$number2;
+   echo "<br>";
+   echo $number1/$number2;
+
+   echo  $perqindje1 % $perqindje2; 
+
+
+   
+
+
+?>
