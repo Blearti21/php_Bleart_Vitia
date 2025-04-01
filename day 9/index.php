411d4cce1f77e398fd2e626e44ce120cd2de009e
Blearti21 <bleartvitia91@gmail.com>
2025-04-01 20:43:01 +0200

    day 9

---
+++ day 9/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 9</title>
+</head>
+<body>
+
+<form aciton="add.php" method="POST">
+    <input type="text" name="name" placeholder="enter your name" -br>
+    <input type="text" name="surname" placeholder="enter your surname"-br>
+    <input type="text" name="email" placeholder="enter your email"-br>
+
+    <button type="submit" name="submit">Submit</button>
+</form>
+
+
+    
+</body>
+</html>
