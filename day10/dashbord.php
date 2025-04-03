1c0deeda3a179398aa30eff6047da8becf6c80ea
Blearti21 <bleartvitia91@gmail.com>
2025-04-03 20:44:30 +0200

    day 10

---
+++ day10/dashbord.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+
+    include_once("config.php");
+    $sql = "select * from user";
+    $getUser = $conn->prepare($sql);
+
+    $getUser->execute();
+
+    $user=$getUser->fetchAll();
+
+
+
+?>
+
+  <table>
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Email</th>
+    </thead>
+    
+    <tbody>
+        <?php
+            foreach($user as $user){
+                ?>
+            <tr>
+                <td><?= $user['id']?></td>
+                <td><?= $user['name']?></td>
+                <td><?= $user['surname']?></td>
+                <td><?= $user['email']?></td>
+            </tr>
+            <?php } ?>
+
+
+    </tbody>
+
+    </table>
+
+    <a href="index.php">go to indeksss</a>
+
+</body>
+</html>
