158e42383db1f417d9148a8ff25ed9df1835690c
Blearti21 <bleartvitia91@gmail.com>
2025-04-17 20:47:35 +0200

    day 12

---
+++ day12/config.php
@@ -0,0 +1,22 @@
+<?php
+   
+    session_start();
+
+    $user="root";
+    $pass="";
+    $server="localhost";
+    $dbname="dbbleart";
+
+
+    try{
+        
+        $conn= new PDO("mysql:host=$server; dbname=$dbname",$user,$pass );
+
+        echo "connected";
+
+    }catch(PDOException $e){
+        echo "error : ". $e->getMessage();
+    }
+
+
+?>
